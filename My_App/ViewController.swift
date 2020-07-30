5138c4d726b0a5c545a439153fd50a7b676c5ddb
Andrey-Dyutin <dyutin.andrei2016@yandex.ru>
2020-07-30 20:09:29 +0400

    Initial Commit

---
+++ My_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My_App
+//
+//  Created by Андрей Дютин on 30.07.2020.
+//  Copyright © 2020 Андрей Дютин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
