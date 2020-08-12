5933a5bd0abdaef00641f7c01ed1706829383936
Andrey-Dyutin <55854890+Andrey-Dyutin@users.noreply.github.com>
2020-08-12 17:02:24 +0400

    login vk

--- IMMApp/AuthViewController/AuthViewController.swift
+++ IMMApp/AuthViewController/AuthViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AuthViewController: UIViewController {
+    private var authService: AuthService!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        authService = SceneDelegate.shared().authService
+        view.backgroundColor = .red
     }
 
-
+    @IBAction func signInVk(_ sender: UIButton) {
+        authService.wakeUpSession()
+    }
+    
 }
 
