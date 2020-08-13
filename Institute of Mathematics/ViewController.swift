b491bced2e1c6f92a2befd72938d609a87db17df
Andrey-Dyutin <55854890+Andrey-Dyutin@users.noreply.github.com>
2020-08-13 23:08:44 +0400

    Commit 1

--- Institute of Mathematics/ViewController.swift
+++ Institute of Mathematics/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageViewLogotype: UIImageView!
+    @IBOutlet weak var activityIndicatorImageViewLogotype: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        activityIndicatorImageViewLogotype.color = .blue
+        activityIndicatorImageViewLogotype.isHidden = true
+        activityIndicatorImageViewLogotype.hidesWhenStopped = true
+        fetchImageLogotype()
+        
+        
     }
-
-
+    //MARK: -
+    func fetchImageLogotype(){
+        activityIndicatorImageViewLogotype.isHidden = false
+        activityIndicatorImageViewLogotype.startAnimating()
+        guard let urlImageLogotype =
+            URL(string:"https://kpfu.ru/docs/F58293241586/img1198266443.jpg") else { return }
+        let session = URLSession.shared
+        session.dataTask(with: urlImageLogotype) { (data, response, error) in
+            if let data = data,let imageLogotype = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicatorImageViewLogotype.startAnimating()
+                    self.activityIndicatorImageViewLogotype.isHidden = true
+                    self.imageViewLogotype.image = imageLogotype
+                }
+            }
+        }.resume()
+    }
+    
+    
+    
 }
 
