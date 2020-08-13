f7838082e059c81947d52b194ab3cd8061c1a06a
Andrey-Dyutin <55854890+Andrey-Dyutin@users.noreply.github.com>
2020-08-13 18:15:42 +0400

    Initial Commit

---
+++ Institute of Mathematics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Institute of Mathematics
+//
+//  Created by Андрей Дютин on 13.08.2020.
+//  Copyright © 2020 Андрей Дютин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
